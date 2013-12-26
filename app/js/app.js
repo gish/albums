713e4331d59febda13b14e893b6913fedc8c0cbf
Erik Hedberg <erik@webbgaraget.se>
2013-12-26 20:54:53 +0100

    Update layout

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
         $intlContainer;
 
     $(document).ready(function() {
-        $sweContainer = $('#swedish-albums');
-        $intlContainer = $('#intl-albums');
+        $sweContainer = $('#swedish-albums .row .col-md-12');
+        $intlContainer = $('#intl-albums .row .col-md-12');
 
         $sweContainer.append(ar.render('swedish'));
         $intlContainer.append(ar.render('international'));
